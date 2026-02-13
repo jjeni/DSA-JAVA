c076d0d68747cabd10dea9ee09877a05d5f20395
jjeni <jenijofficial@gmail.com>
2026-02-13 11:53:08 +0530

    Solved problem 1

---
+++ day4/AddStrings.java
@@ -0,0 +1,27 @@
+package day4;
+
+import java.lang.classfile.instruction.StackInstruction;
+
+public class AddStrings {
+    static String addStrings(String num1, String num2){
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        int carry = 0;
+        while(i>=0 ||j>=0 || carry !=0){
+            int digit1 = i >= 0 ? num1.charAt(i)-'0' : 0;
+            int digit2 = j >= 0 ? num2.charAt(j)-'0' : 0;
+            int sum = digit1 + digit2 + carry;
+            sb.append(sum%10);
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        String num1 = "188";
+        String num2 = "22";
+        System.out.println(addStrings(num1, num2));
+    }
+}
